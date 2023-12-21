df3ed573c1477cd9337f23cbe9814554cbf23d7f
Emil Wilson <emilu551e@gmail.com>
2023-12-21 14:38:27 +0530

    Implemented socket.io basic functionality of realtime chat ended

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const path=require('path')
+const http=require('http')
+const socketio=require('socket.io')
+
+const app= express()
+const server=http.createServer(app)
+const io=socketio(server)
+
+//Set static folder
+app.use(express.static(path.join(__dirname,"public")))
+
+
+//Run when Client connects
+io.on('connection',socket =>{
+  
+    //Welcome ccurrent user
+    socket.emit('message','Welcome to Chatcord')
+
+    //Broadcast when a user connects
+    socket.broadcast.emit('message','A user has joined the chat');
+
+    //Runs when client disconnects
+    socket.on('disconnect',()=>{
+       io.emit('message','A user has left the chat') 
+    })
+
+    //Listen for chatMessage
+    socket.on('chatMessage',msg=>{
+        io.emit('message',msg)
+    })
+})
+
+const PORT= 3000 || process.env.PORT
+
+server.listen(PORT,()=> console.log(`Server is lisening on ${PORT}`));
